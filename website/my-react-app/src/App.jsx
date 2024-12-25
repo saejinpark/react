3c42fc522f2380440e7073085ce7ad71c58d4707
saejinpark <saejinpark951210@gmail.com>
2024-12-25 20:27:48 +0900

    Bro Code React 1:30 milestonee

--- website/my-react-app/src/App.jsx
+++ website/my-react-app/src/App.jsx
@@ -1,11 +1,29 @@
-import UserGreeting from './UserGreeting.jsx'
+import List from './List'
 
 function App() {
+  const fruits = [
+      {id: 1, name: "apple", calories: 95},
+      {id: 2, name: "orange", calories: 45},
+      {id: 3, name: "banana", calories: 105},
+      {id: 4, name: "coconut", calories: 159},
+      {id: 5, name: "pineapple", calories: 37}
+  ]
+
+  const vegetables = [
+    {id: 6, name: "potato", calories: 110},
+    {id: 7, name: "celery", calories: 15},
+    {id: 8, name: "carrots", calories: 25},
+    {id: 9, name: "corn", calories: 63},
+    {id: 10, name: "broccoli", calories: 50}
+  ]
 
 
   return (
     <>
-      <UserGreeting isLoggedIn={false} username="BroCode" />
+      {fruits.length > 0 ? <List items={fruits} category="Fruits"></List> : null}
+      {vegetables.length > 0 ?  <List items={vegetables} category="vegetables"></List> : null}
+      
+     
     </>
   );
 }
